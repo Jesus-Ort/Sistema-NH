1ce87f2a6e45ce679796ec841b20d1596b6284a1
JesusOrt <xJesus_Nevermor3@hotmail.com>
2025-06-21 17:25:42 -0400

    Barra de navegacion terminada, el drawer se debe mejorar con el backend para gestionar con roles

--- Frontend SNH/src/components/NavBar.vue
+++ Frontend SNH/src/components/NavBar.vue
@@ -1,17 +1,39 @@
 <template>
     <v-app-bar color="primary" dark app>
+    <!-- Izquierda: Drawer -->
+    <div class="d-flex align-center">
         <v-app-bar-nav-icon @click="$emit('toggle-drawer')" />
-        <v-divider vertical class="ma-1"/>
-        <v-toolbar-title>Convit</v-toolbar-title>
-        <v-divider vertical class="ma-1"/>
+    </div>
+    <v-divider vertical></v-divider>
+    <!-- Spacer para empujar el logo al centro -->
+    <v-spacer />
+    <!-- Centro: Logo -->
+    <div class="d-flex align-center">
+        <Logo width="100" />
+    </div>
+    <!-- Spacer para empujar los botones a la derecha -->
+    <v-spacer />
+    <v-divider vertical></v-divider>
+    <!-- Derecha: Iconos -->
+    <div class="d-flex align-center">
         <v-btn class="ma-1" icon to="/home">
             <v-icon>mdi-home</v-icon>
         </v-btn>
+        <v-divider vertical></v-divider>
         <v-btn class="ma-1" icon to="/pacientes">
             <v-icon>mdi-account-group</v-icon>
         </v-btn>
+        <v-divider vertical></v-divider>
         <v-btn class="ma-1" icon to="/vacunas">
             <v-icon>mdi-needle</v-icon>
         </v-btn>
+        <v-divider vertical></v-divider>
+        <darkMode class="ma-1"/>
+        </div>
     </v-app-bar>
 </template>
+
+<script setup>
+import Logo from "@/assets/logo.svg";
+import darkMode from "@/components/DarkModeToggle.vue"
+</script>
