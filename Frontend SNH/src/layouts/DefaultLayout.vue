daea0b94a5ee4c84cce2464e30e1780f225168d2
JesusOrt <xJesus_Nevermor3@hotmail.com>
2025-06-21 21:14:03 -0400

    Drawer v2

--- Frontend SNH/src/layouts/DefaultLayout.vue
+++ Frontend SNH/src/layouts/DefaultLayout.vue
@@ -3,17 +3,25 @@
     <NavBar @toggle-drawer="drawer = !drawer"/>
 
     <v-navigation-drawer app v-model="drawer">
-      <v-list nav dense>
-        <v-list-item
-          v-for="item in drawerItems"
-          :key="item.to"
-          :to="item.to"
-          :prepend-icon="item.icon"
-          link
-        >
-          <v-list-item-title>{{ item.label }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
+        <v-list nav dense>
+            <template v-for="section in drawerItems" :key="section.title || 'inicio'">
+              <template v-if="section.title">
+                <v-divider class="my-2" />
+                <v-list-subheader class="text-uppercase">
+                  {{ section.title }}
+                </v-list-subheader>
+              </template>
+              <v-list-item
+                v-for="item in section.items"
+                :key="item.to"
+                :to="item.to"
+                :prepend-icon="item.icon"
+                link
+              >
+                <v-list-item-title>{{ item.label }}</v-list-item-title>
+              </v-list-item>
+            </template>
+        </v-list>
     </v-navigation-drawer>
 
     <v-main class="pa-4 mt-15">
@@ -30,15 +38,33 @@ const drawer = ref(false)
 const userRole = ref('administrativo') // Sacar del Backend.
 
 const allDrawerItems = [
-  { label: 'Inicio', icon: 'mdi-home', to: '/home', roles: ['administrativo', 'salud'] },
-  { label: 'Pacientes', icon: 'mdi-account-group', to: '/pacientes', roles: ['salud'] },
-  { label: 'Vacunas', icon: 'mdi-needle', to: '/vacunas', roles: ['salud'] },
-  { label: 'Usuarios', icon: 'mdi-account-cog', to: '/usuarios', roles: ['administrativo'] },
-  { label: 'Reportes', icon: 'mdi-file-chart', to: '/reportes', roles: ['administrativo'] },
-  { label: 'Perfil', icon: 'mdi-account', to: '/perfil', roles: ['administrativo', 'salud'] },
+  {
+    title: null,
+    items: [
+      { label: 'Inicio', icon: 'mdi-home', to: '/home', roles: ['administrativo', 'salud'] },
+      { label: 'Reportes', icon: 'mdi-file-chart', to: '/reportes', roles: ['administrativo'] },
+    ],
+  },
+  {
+    title: 'Registro',
+    items: [
+      { label: 'Registrar Paciente', icon: 'mdi-account-group', to: '/pacientes', roles: ['administrativo'] },
+      { label: 'Registrar Vacuna', icon: 'mdi-needle', to: '/vacunas', roles: ['administrativo'] },
+      { label: 'Registrar Usuario', icon: 'mdi-account-cog', to: '/usuarios', roles: ['administrativo'] },
+      { label: 'Registrar Dosis', icon: 'mdi-hospital', to: '/dosis', roles: ['administrativo', 'salud'] },
+    ],
+  },
 ]
 
 const drawerItems = computed(() =>
-  allDrawerItems.filter(item => item.roles.includes(userRole.value))
+  allDrawerItems
+    .map(section => {
+      const filteredItems = section.items.filter(i => i.roles.includes(userRole.value))
+      return {
+        title: section.title,
+        items: filteredItems,
+      }
+    })
+    .filter(section => section.items.length > 0)
 )
 </script>
